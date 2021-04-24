b08b89196c9b33e7e27a4cb5f32d8cd788b7a601
piercepatrick <piercepatrick2001@gmail.com>
2021-04-24 12:17:23 -0500

    Set up Firebase and add phone verification buttons

--- whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainActivity.java
+++ whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.whatsapp_clone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.code);
+        mSend = findViewById(R.id.send);
+
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPhoneNumberVerification();
+            }
+        });
+
+    }
+
+    private void startPhoneNumberVerification() {
+        //PhoneAuthProvider
     }
 }

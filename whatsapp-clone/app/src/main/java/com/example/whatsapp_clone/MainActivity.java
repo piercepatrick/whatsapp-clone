b69e7dbe50f1278ac496d238487b044bf8755791
piercepatrick <piercepatrick2001@gmail.com>
2021-04-26 13:08:31 -0500

    Add logout button

--- whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainActivity.java
+++ whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.whatsapp_clone;
 
-import androidx.annotation.NonNull;
+import  androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
--- whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainPageActivity.java
+++ whatsapp-clone/app/src/main/java/com/example/whatsapp_clone/MainPageActivity.java
@@ -2,7 +2,12 @@ package com.example.whatsapp_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPageActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        Button mLogout = findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
